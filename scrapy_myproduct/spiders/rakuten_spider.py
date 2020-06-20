5f8fac538a7f54b6e03ee9b582ad54027e5e7ffb
車東秀 <cds2363@gmail.com>
2020-06-20 16:49:24 +0900

    サンプル

--- scrapy_myproduct/spiders/rakuten_spider.py
+++ scrapy_myproduct/spiders/rakuten_spider.py
@@ -3,9 +3,12 @@ import scrapy
 
 
 class RakutenSpiderSpider(scrapy.Spider):
-    name = 'rakuten_spider'
-    allowed_domains = ['rakuten.co.jp']
-    start_urls = ['http://rakuten.co.jp/']
+    name = 'SearchRakutenProduct'
+    allowed_domains = ['search.rakuten.co.jp']
+    start_urls = ['https://search.rakuten.co.jp/search/mall/uc-0358/']
 
     def parse(self, response):
+        for item in response.xpath('//div[@class="dui-card searchresultitem"]'):
+            item.xpath('//div[@class="content title"]/h2/a').get()
+            
         pass
